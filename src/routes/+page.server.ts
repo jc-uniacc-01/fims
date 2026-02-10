ce50e4fbb131aa00d0edf1e75160a21081cd4b8b
nousername010 <gdemail2025@proton.me>
2026-02-10 21:39:08 +0800

    chore(page.server.ts) change test

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,20 +4,6 @@ import {db} from "$lib/server/db";
 import { accountRoles } from "$lib/server/schema";
 import * as accountDAO from "$lib/server/accountDAO";
 
-db.insert(accountRoles).values({
-    accountRole: "testRole",
-    "canAddAccount": 0,
-    "canViewChangeLogs": 0,
-    "canAddFaculty": 0,
-    "canModifyAccount": 0,
-    "canModifyFaculty": 0,
-});
-
-const result = await db
-    .select()
-    .from(accountRoles)
-console.log(result)
-
-accountDAO.deleteAcc(1);
+console.log(accountDAO.listAll());
 
 console.log ("Page loaded")
