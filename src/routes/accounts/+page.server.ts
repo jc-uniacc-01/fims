a03d851968edd1bf4b88a4a7e90696531975895f
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-12 03:00:28 +0800

    feat(accounts): make form action for account creation

---
+++ src/routes/accounts/+page.server.ts
@@ -0,0 +1,50 @@
+import type { Actions } from "@sveltejs/kit";
+import { fail } from "@sveltejs/kit";
+import { auth } from "$lib/server/auth";
+
+export const actions = {
+  default: async ({ request }) => {
+    const data = await request.formData();
+		const email = data.get('email') as string;
+		const password = data.get('password') as string;
+    const role = data.get('role') as string;
+
+    // Validate credentials
+    if (!email || !email.endsWith('@up.edu.ph')) {
+      return fail(400, { error: 'Invalid email.' })
+    }
+
+    if (!password) {
+      return fail(400, { error: 'Invalid password.' });
+    }
+
+    if (!role) {
+      return fail(400, { error: 'Invalid role.' });
+    }
+
+    // Register as user
+    let responseUser = undefined;
+    try {
+      const response = await auth.api.signUpEmail({
+        body: {
+          email,
+          password,
+          name: 'User',
+        },
+      });
+
+      responseUser = response.user;
+    } catch (error) {
+      return fail(500, { error: 'Failed to make new account.' });
+    }
+
+    if (!responseUser) {
+      return fail(500, { error: 'Failed to make new account.' });
+    }
+
+    return {
+      success: true,
+      message: 'Created account.',
+    };
+  }
+} satisfies Actions
