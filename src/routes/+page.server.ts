72497ad4aa919cbca223106f80bfb3ea3e132fbe
nousername010 <gdemail2025@proton.me>
2026-02-12 18:45:15 +0800

    refactor: changed to use better-auth

--- src/routes/+page.server.ts
+++
@@ -1,8 +0,0 @@
-// Doing this in order to test db code
-
-import * as accountDAO from "$lib/server/accountDAO";
-
-let results = await accountDAO.listAll();
-console.log(results);
-
-console.log ("Page loaded")
