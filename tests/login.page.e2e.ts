fe4d154981e78941a3f051fe4f87ac8402c919bd
nousername010 <gdemail2025@proton.me>
2026-02-12 23:32:01 +0800

    add: add account access tests

--- tests/login.page.e2e.ts
+++ tests/login.page.e2e.ts
@@ -44,3 +44,37 @@ test('failed login', async ({page}) => {
     //change this expect when the warning has an id
     await expect(page.locator("p.px-8")).toHaveText("Invalid email or password")
 });
+
+test('attempt to view account as IT (or with account perms)', async ({page}) => {
+    await page.goto("/login");
+
+    await expect(page.locator("#email")).toBeVisible();
+    await expect(page.locator("#password")).toBeVisible();
+
+    await page.locator("#email").fill("testadmin@up.edu.ph")
+    await page.locator("#password").fill("adminpass")
+    await page.locator("#password").press("Enter")
+
+    await expect(page).not.toHaveURL(FAIL_ROUTE);
+    await expect(page.locator(".text-fims-green")).toBeVisible;
+
+    await page.getByText("Accounts").click()
+    expect(page.getByText("404")).not.toBeVisible;
+})
+
+test('attempt to view account as admin (or without account perm)', async ({page}) => {
+    await page.goto("/login");
+
+    await expect(page.locator("#email")).toBeVisible();
+    await expect(page.locator("#password")).toBeVisible();
+
+    await page.locator("#email").fill("testadmin@up.edu.ph")
+    await page.locator("#password").fill("adminpass")
+    await page.locator("#password").press("Enter")
+
+    await expect(page).not.toHaveURL(FAIL_ROUTE);
+    await expect(page.locator(".text-fims-green")).toBeVisible;
+
+    await page.getByText("Accounts").click()
+    expect(page.getByText("404")).toBeVisible;
+})
