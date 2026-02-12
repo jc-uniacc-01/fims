2d2147677789f4fe3491447451224a4f3994ded9
nousername010 <gdemail2025@proton.me>
2026-02-12 22:47:01 +0800

    add: init creation and deletion test

--- tests/account.test.ts
+++ tests/account.test.ts
@@ -1,15 +1,19 @@
-import { expect, describe, beforeEach, beforeAll, afterEach, afterAll } from "vitest"
+import { expect, test, beforeEach, beforeAll, afterEach, afterAll } from "vitest"
 import { db } from "../src/lib/server/db"
+import * as DBHelper from "$lib/server/db-helpers"
 import { appuser } from "../src/lib/server/db/schema"
 import { eq } from "drizzle-orm"
 import * as AccountDAO from "../src/lib/server/accountDAO"
+import { withNeonTestBranch } from "./backend/test-setup"
 
-describe("account listing", async () => {
+test("account listing", async () => {
     let accounts = await AccountDAO.listAll()
     expect(accounts.length).toBeGreaterThan(0)
-    expect(accounts.find((elem) => {elem.accountId == "oP77pgWyY6fcUjyanDUbyPOWOr9eNSt1"})).not.toBe([]);
+    expect(accounts.find((elem) => {elem.accountId == "oP77pgWyY6fcUjyanDUbyPOWOr9eNSt1"})).not.toBe([]); //find test account
 });
 
-describe("account creation and deletion", async () => {
-
+test("account creation and deletion", async () => {
+    expect(await AccountDAO.createAcc("vitest@up.edu.ph", "Vitest Tester", "password", "IT")).toBeTruthy;
+    let accounts = await AccountDAO.listAll() 
+    expect(accounts.find((elem) => {elem.accountId == "oP77pgWyY6fcUjyanDUbyPOWOr9eNSt1"})).not.toBe([]); //find test account
 });
