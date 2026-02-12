ffa750e1690dd34a06f85c6d3e4e49a031a45e4a
nousername010 <gdemail2025@proton.me>
2026-02-12 17:09:39 +0800

    chore: update test

--- tests/login/login.spec.ts
+++ tests/login/login.spec.ts
@@ -1,6 +1,12 @@
 import { test, expect } from "@playwright/test"
 
 test('successful login', async ({ page }) => {
-    page.goto("/login");
-    
+    await page.goto("/login");
+
+    await expect(page.locator("#email")).toBeVisible()
+    await expect(page.locator("#password")).toBeVisible()
+
+    await page.locator("#email").fill("testaccount@up.edu.ph")
+    await page.locator("#password").fill("password")
+    await page.locator("#password").press("Enter")
 });
