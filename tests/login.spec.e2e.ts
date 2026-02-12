6aa1090628fd62989ece8ce2825457c33c9199fb
nousername010 <gdemail2025@proton.me>
2026-02-12 18:56:28 +0800

    add: finish login test

--- tests/login.spec.e2e.ts
+++ tests/login.spec.e2e.ts
@@ -6,7 +6,9 @@ test('successful login', async ({ page }) => {
     await expect(page.locator("#email")).toBeVisible()
     await expect(page.locator("#password")).toBeVisible()
 
-    await page.locator("#email").fill("testaccount@up.edu.ph")
+    await page.locator("#email").fill("testacc@up.edu.ph")
     await page.locator("#password").fill("password")
     await page.locator("#password").press("Enter")
+
+    await expect(page).toHaveURL("/");
 });
