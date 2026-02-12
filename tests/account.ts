d7cb190cfa03329245ec61bdcea46d79b567830f
nousername010 <gdemail2025@proton.me>
2026-02-12 19:59:46 +0800

    chore: mroe init

---
+++ tests/account.ts
@@ -0,0 +1,17 @@
+import { expect, test, beforeEach, afterEach } from "vitest"
+import { db } from "../src/lib/server/db"
+import * as AccountDAO from "../src/lib/server/accountDAO"
+
+let prevState;
+
+test("account creation", () => {
+
+})
+
+test("account deletion", () => {
+
+})
+
+test("account listing", () => {
+
+})
