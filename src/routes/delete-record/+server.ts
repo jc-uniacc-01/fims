42ad2c07ea6999e30b1ee191310c61b61ce313e0
nousername010 <gdemail2025@proton.me>
2026-02-23 21:36:01 +0800

    feat: secure endpoints

--- src/routes/delete-record/+server.ts
+++ src/routes/delete-record/+server.ts
@@ -1,11 +1,15 @@
 import { deleteFacultyRecord } from '$lib/server/db-helpers';
-import { json } from '@sveltejs/kit';
+import { json, error } from '@sveltejs/kit';
 
 // TODO: secure with cookies
-export async function POST({request, cookies}) {
+export async function POST({request, locals}) {
+    if (!locals.user || !locals.session) {
+        throw error(401, 'unauthorized');
+    }
+
     const data = await request.json();
-    if (!data) return Response.error();
-    if (!data.deleted) return Response.error();
+    if (!data) throw error(403, 'malformed data');
+    if (!data.deleted) throw error(403, 'malformed data');
     await deleteFacultyRecord(data.deleted);
     return json({success: true});;
 }
--- src/routes/view-record/+server.ts
+++ src/routes/view-record/+server.ts
@@ -1,8 +1,17 @@
-import { json, redirect } from '@sveltejs/kit'
+import { error, redirect } from '@sveltejs/kit'
 
-export const POST = async ({request, cookies}) => {
+export const POST = async ({request, cookies, locals}) => {
+    if (!locals.user || !locals.session) {
+        throw error(401, 'unauthorized');
+    }
+
     const data:{requested:number} = await request.json();
+    
+    if (!data) throw error(403, 'malformed data');
+    if (!data.requested) throw error(403, 'malformed data');
+
     cookies.set('viewed-record', `${data.requested}`, {path: '/'}); 
 
     redirect(302, '/record');
+
 }
