216cbd81d8676caf079e95b8107bd5cd7ccac117
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-27 00:10:13 +0800

    test(account-actions.e2e): test batch account deletion and filtration

--- tests/playwright/it-specific/account-actions.e2e.ts
+++ tests/playwright/it-specific/account-actions.e2e.ts
@@ -7,6 +7,9 @@ test.use({ storageState: 'playwright/.auth/it.json' });
 const dummyEmail = process.env.DUMMY_EMAIL!;
 const dummyPw = process.env.DUMMY_PASS!;
 
+const dummyEmail1 = process.env.DUMMY_EMAIL1!;
+const dummyEmail2 = process.env.DUMMY_EMAIL2!;
+
 test.describe('add account', () => {
     test.describe('unsuccessful', () => {
         test('no email', async ({ page }) => {
@@ -172,5 +175,328 @@ test.describe('delete account', () => {
         // The new email should no longer be visible
         const cell = page.getByText(dummyEmail);
         await expect(cell).not.toBeVisible();
+    });
+});
+
+test.describe('batch delete accounts', () => {
+    test('make zeroth account', async ({ page }) => {
+        // No redirection since user is logged-in
+        page.goto('/accounts');
+        await expect(page).toHaveURL('/accounts');
+
+        // Made
+
+        // Add Account
+        await page.getByRole('button', { name: '+ Add Account', exact: true }).click();
+
+        // Input
+
+        // Email
+        const emailInput = page.getByRole('textbox', { name: 'Enter email here', exact: true });
+        await expect(emailInput).toBeEmpty();
+        await expect(emailInput).toBeEditable();
+        await emailInput.fill(dummyEmail);
+
+        // Password
+        const pwInput = await page.getByRole('textbox', { name: 'Set initial password', exact: true });
+        await expect(pwInput).toBeEmpty();
+        await expect(pwInput).toBeEditable();
+        await pwInput.fill(dummyPw);
+
+        // Save Account
+        await page.getByRole('button', { name: 'Save', exact: true }).click();
+
+        // Confirm
+        await page.getByRole('button', { name: 'Save', exact: true }).last().click();
+
+        // Check message
+        const afterMakeMessage = await page.getByText('Created account.');
+        await expect(afterMakeMessage).toBeVisible();
+
+        // The new email should be visible
+        const cell = page.getByText(dummyEmail);
+        await expect(cell).toBeVisible();
+    });
+
+    test('make first account', async ({ page }) => {
+        // No redirection since user is logged-in
+        page.goto('/accounts');
+        await expect(page).toHaveURL('/accounts');
+
+        // Made
+
+        // Add Account
+        await page.getByRole('button', { name: '+ Add Account', exact: true }).click();
+
+        // Input
+
+        // Email
+        const emailInput = page.getByRole('textbox', { name: 'Enter email here', exact: true });
+        await expect(emailInput).toBeEmpty();
+        await expect(emailInput).toBeEditable();
+        await emailInput.fill(dummyEmail1);
+
+        // Password
+        const pwInput = await page.getByRole('textbox', { name: 'Set initial password', exact: true });
+        await expect(pwInput).toBeEmpty();
+        await expect(pwInput).toBeEditable();
+        await pwInput.fill(dummyPw);
+
+        // Save Account
+        await page.getByRole('button', { name: 'Save', exact: true }).click();
+
+        // Confirm
+        await page.getByRole('button', { name: 'Save', exact: true }).last().click();
+
+        // Check message
+        const afterMakeMessage = await page.getByText('Created account.');
+        await expect(afterMakeMessage).toBeVisible();
+
+        // The new email should be visible
+        const cell = page.getByText(dummyEmail1);
+        await expect(cell).toBeVisible();
+    });
+
+    test('make second account', async ({ page }) => {
+        // No redirection since user is logged-in
+        page.goto('/accounts');
+        await expect(page).toHaveURL('/accounts');
+
+        // Made
+
+        // Add Account
+        await page.getByRole('button', { name: '+ Add Account', exact: true }).click();
+
+        // Input
+
+        // Email
+        const emailInput = page.getByRole('textbox', { name: 'Enter email here', exact: true });
+        await expect(emailInput).toBeEmpty();
+        await expect(emailInput).toBeEditable();
+        await emailInput.fill(dummyEmail2);
+
+        // Password
+        const pwInput = await page.getByRole('textbox', { name: 'Set initial password', exact: true });
+        await expect(pwInput).toBeEmpty();
+        await expect(pwInput).toBeEditable();
+        await pwInput.fill(dummyPw);
+
+        // Save Account
+        await page.getByRole('button', { name: 'Save', exact: true }).click();
+
+        // Confirm
+        await page.getByRole('button', { name: 'Save', exact: true }).last().click();
+
+        // Check message
+        const afterMakeMessage = await page.getByText('Created account.');
+        await expect(afterMakeMessage).toBeVisible();
+
+        // The new email should be visible
+        const cell = page.getByText(dummyEmail2);
+        await expect(cell).toBeVisible();
+    });
+
+    test('filter', async ({ page }) => {
+        // No redirection since user is logged-in
+        page.goto('/accounts');
+        await expect(page).toHaveURL('/accounts');
+
+        // Expect all dummies are visible
+        const adminCell = page.getByText(process.env.ADMIN_EMAIL!);
+        await expect(adminCell).toBeVisible();
+
+        const cell = page.getByText(dummyEmail);
+        await expect(cell).toBeVisible();
+
+        const cell1 = page.getByText(dummyEmail1);
+        await expect(cell1).toBeVisible();
+    
+        const cell2 = page.getByText(dummyEmail2);
+        await expect(cell2).toBeVisible();
+
+        // Select role filter
+        await page.getByRole('button', { name: 'Role' }).first().click();
+        await page.getByRole('button', { name: 'IT' }).first().click();
+        await page.getByRole('button', { name: 'Role' }).first().click();
+
+        // Expect all dummies except for admin are visible
+        await expect(adminCell).not.toBeVisible();
+        await expect(cell).toBeVisible();
+        await expect(cell1).toBeVisible();
+        await expect(cell2).toBeVisible();
+
+        // Unselect role filter
+        await page.getByRole('button', { name: 'Role' }).first().click();
+        await page.getByRole('button', { name: 'IT' }).nth(1).click();
+        await page.getByRole('button', { name: 'Role' }).first().click();
+
+        // Expect all dummies are visible
+        await expect(adminCell).toBeVisible();
+        await expect(cell).toBeVisible();
+        await expect(cell1).toBeVisible();
+        await expect(cell2).toBeVisible();
+    });
+
+    test('check selection', async ({ page }) => {
+        // No redirection since user is logged-in
+        page.goto('/accounts');
+        await expect(page).toHaveURL('/accounts');
+
+        // Select an account
+        const checkbox1 = await page.getByRole('checkbox').first();
+        expect(checkbox1).not.toBeChecked();
+        await checkbox1.check();
+        expect(checkbox1).toBeChecked();
+
+        // See if the buttons are showing
+        const selectAllBtn = await page.getByRole('button', { name: 'Select All', exact: true });
+        expect(selectAllBtn).toBeVisible();
+        
+        const deselectSelectionBtn = await page.getByRole('button', { name: 'Deselect Selection', exact: true });
+        expect(deselectSelectionBtn).toBeVisible();
+
+        const deleteRecordsBtn = await page.getByRole('button', { name: 'Delete Account', exact: true });
+        expect(deleteRecordsBtn).toBeVisible();
+
+        // Select All
+        await selectAllBtn.click();
+
+        const checkbox2 = await page.getByRole('checkbox').nth(1);
+        expect(checkbox2).toBeChecked();
+
+        const checkbox3 = await page.getByRole('checkbox').nth(2);
+        expect(checkbox3).toBeChecked();
+
+        const checkbox4 = await page.getByRole('checkbox').nth(3);
+        expect(checkbox4).toBeChecked();
+
+        // Unselect one checkbox
+        await checkbox1.click();
+        expect(checkbox1).not.toBeChecked();
+
+        // Deselect Selection
+        await deselectSelectionBtn.click();
+        expect(checkbox1).not.toBeChecked();
+        expect(checkbox2).not.toBeChecked();
+        expect(checkbox3).not.toBeChecked();
+        expect(checkbox4).not.toBeChecked();
+
+        expect(selectAllBtn).not.toBeVisible();
+        expect(deselectSelectionBtn).not.toBeVisible();
+        expect(deleteRecordsBtn).not.toBeVisible();
+    });
+
+    test('cancelled deletion', async ({ page }) => {
+        // No redirection since user is logged-in
+        page.goto('/accounts');
+        await expect(page).toHaveURL('/accounts');
+
+        // Select Accounts
+        const checkbox1 = await page.getByRole('checkbox').first();
+        expect(checkbox1).not.toBeChecked();
+        await checkbox1.check();
+        expect(checkbox1).toBeChecked();
+
+        const selectAllBtn = await page.getByRole('button', { name: 'Select All', exact: true });
+        expect(selectAllBtn).toBeVisible();
+        await selectAllBtn.click();
+
+        const checkbox2 = await page.getByRole('checkbox').nth(1);
+        expect(checkbox2).toBeChecked();
+
+        const checkbox3 = await page.getByRole('checkbox').nth(2);
+        expect(checkbox3).toBeChecked();
+
+        const checkbox4 = await page.getByRole('checkbox').nth(3);
+        expect(checkbox4).toBeChecked();
+
+        await checkbox1.click();
+        expect(checkbox1).not.toBeChecked();
+
+        // Delete Accounts
+        const deleteRecordsBtn = await page.getByRole('button', { name: 'Delete Accounts', exact: true });
+        expect(deleteRecordsBtn).toBeVisible();
+        await deleteRecordsBtn.click();
+
+        // Don't confirm
+        await page.getByRole('button', { name: 'Cancel', exact: true }).click();
+
+        // The new records should still be visible
+        const cell = page.getByText(process.env.ADMIN_EMAIL!);
+        await expect(cell).toBeVisible();
+
+        const cell1 = page.getByText(dummyEmail);
+        await expect(cell1).toBeVisible();
+    
+        const cell2 = page.getByText(dummyEmail1);
+        await expect(cell2).toBeVisible();
+
+        const cell3 = page.getByText(dummyEmail2);
+        await expect(cell3).toBeVisible();
+
+        // Deselect Selection
+        const deselectSelectionBtn = await page.getByRole('button', { name: 'Deselect Selection', exact: true });
+        expect(deselectSelectionBtn).toBeVisible();
+
+        await deselectSelectionBtn.click();
+        expect(checkbox1).not.toBeChecked();
+        expect(checkbox2).not.toBeChecked();
+        expect(checkbox3).not.toBeChecked();
+        expect(checkbox4).not.toBeChecked();
+
+        expect(selectAllBtn).not.toBeVisible();
+        expect(deselectSelectionBtn).not.toBeVisible();
+        expect(deleteRecordsBtn).not.toBeVisible();
+    });
+    
+    test('deleted', async ({ page }) => {
+        // No redirection since user is logged-in
+        page.goto('/accounts');
+        await expect(page).toHaveURL('/accounts');
+
+        // Select Accounts
+        const checkbox1 = await page.getByRole('checkbox').first();
+        expect(checkbox1).not.toBeChecked();
+        await checkbox1.check();
+        expect(checkbox1).toBeChecked();
+
+        const selectAllBtn = await page.getByRole('button', { name: 'Select All', exact: true });
+        expect(selectAllBtn).toBeVisible();
+        await selectAllBtn.click();
+
+        const checkbox2 = await page.getByRole('checkbox').nth(1);
+        expect(checkbox2).toBeChecked();
+
+        const checkbox3 = await page.getByRole('checkbox').nth(2);
+        expect(checkbox3).toBeChecked();
+
+        const checkbox4 = await page.getByRole('checkbox').nth(3);
+        expect(checkbox4).toBeChecked();
+
+        await checkbox1.click();
+        expect(checkbox1).not.toBeChecked();
+
+        const cell = page.getByText(process.env.ADMIN_EMAIL!);
+        const cell1 = page.getByText(dummyEmail);
+        const cell2 = page.getByText(dummyEmail1);
+        const cell3 = page.getByText(dummyEmail2);
+
+        // Delete Accounts
+        const deleteRecordsBtn = await page.getByRole('button', { name: 'Delete Accounts', exact: true });
+        expect(deleteRecordsBtn).toBeVisible();
+        await deleteRecordsBtn.click();
+
+        // Confirm
+        await page.getByRole('button', { name: 'Delete', exact: true }).last().click();
+
+        // Check message
+        const afterDeleteMessage = await page.getByText('Deleted accounts.');
+        await expect(afterDeleteMessage).toBeVisible();
+
+        // The new records should not be visible
+        await expect(cell).toBeVisible();
+        await expect(cell1).not.toBeVisible();
+        await expect(cell2).not.toBeVisible();
+        await expect(cell3).not.toBeVisible();
     });
 });
