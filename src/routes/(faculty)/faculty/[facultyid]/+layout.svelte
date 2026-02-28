1d42161074f52ac8ad020f7e272cd4b8966c8368
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-28 18:31:57 +0800

    refactor(faculty): move buttons to layout

--- src/routes/(faculty)/faculty/[facultyid]/+layout.svelte
+++ src/routes/(faculty)/faculty/[facultyid]/+layout.svelte
@@ -1,8 +1,32 @@
 <script lang="ts">
     import Icon from '@iconify/svelte';
+    import GreenButton from '$lib/ui/GreenButton.svelte';
+    import RedButton from '$lib/ui/RedButton.svelte';
+    import DeleteConfirmation from '$lib/ui/DeleteConfirmation.svelte';
+    import LoadingScreen from '$lib/ui/LoadingScreen.svelte';
+
+    import { enhance } from '$app/forms';
+    import { page } from '$app/state';
+    import { setContext } from 'svelte';
+    import { writable } from 'svelte/store';
 
     const { data, children } = $props();
     const { lastName, firstName } = $derived(data);
+
+    const currentAcadYear = (new Date).getFullYear();
+    const currentSemNum = 2; // TODO: Find a better way to know current semester
+
+    // Store the chosen academic year and semester for semestral records
+    const acadYearStore = writable(currentAcadYear);
+    const semNumStore = writable(currentSemNum);
+
+    // Propagate the link to the child semestral records
+    setContext('semestralRecord', { acadYearStore, semNumStore });
+
+    let willDelete = $state(false);
+    let isLoading = $state(false);
+
+    let deleteForm: HTMLFormElement | null = $state(null);
 </script>
 
 <main class="bg-[#e9e9e9]">
@@ -16,6 +40,48 @@
                 {lastName}, {firstName}
             </h1>
         </div>
+
+        <div class="mt-4 flex justify-between">
+            <GreenButton>
+                <Icon icon="tabler:file-export" class="mr-2 h-5 w-5" />
+                <span>Export Reports</span>
+            </GreenButton>
+            <RedButton onclick={() => (willDelete = true)}>
+                <Icon icon="tabler:trash" class="mr-2 h-6 w-6" />
+                <span>Delete Record</span>
+            </RedButton>
+        </div>
+
         {@render children()}
     </div>
 </main>
+
+{#if willDelete}
+    <form
+        method="post"
+        action="?/delete"
+        bind:this={deleteForm}
+        use:enhance={() => {
+            willDelete = false;
+            isLoading = true;
+            return async ({ update }) => {
+                await update();
+                isLoading = false;
+            };
+        }}
+    >
+        <input type="hidden" name="facultyid" value={page.params.facultyid} />
+
+        <DeleteConfirmation
+            onDelete={() => {
+                if (deleteForm) deleteForm.requestSubmit();
+            }}
+            onCancel={() => (willDelete = false)}
+            text="This will delete the current faculty record. Are you sure?"
+        />
+    </form>
+{/if}
+
+{#if isLoading}
+    <LoadingScreen />
+{/if}
--- src/routes/(faculty)/faculty/[facultyid]/+page.svelte
+++ src/routes/(faculty)/faculty/[facultyid]/+page.svelte
@@ -1,20 +1,8 @@
 <script lang="ts">
     import Icon from '@iconify/svelte';
-    import GreenButton from '$lib/ui/GreenButton.svelte';
-    import RedButton from '$lib/ui/RedButton.svelte';
-    import DeleteConfirmation from '$lib/ui/DeleteConfirmation.svelte';
-    import LoadingScreen from '$lib/ui/LoadingScreen.svelte';
-
-    import { enhance } from '$app/forms';
-    import { page } from '$app/state';
 
     const { data, form } = $props();
     const { profile } = $derived(data);
-
-    let willDelete = $state(false);
-    let isLoading = $state(false);
-
-    let deleteForm: HTMLFormElement | null = $state(null);
 </script>
 
 {#if form?.error}
@@ -24,47 +12,4 @@
         <Icon icon="tabler:alert-hexagon" class="h-6 w-6 text-fims-red" />
         <p class="px-8">{form.error}</p>
     </div>
-{/if}
-
-<div>
-    <div class="mt-4 flex justify-between">
-        <GreenButton>
-            <Icon icon="tabler:file-export" class="mr-2 h-5 w-5" />
-            <span>Export Reports</span>
-        </GreenButton>
-        <RedButton onclick={() => (willDelete = true)}>
-            <Icon icon="tabler:trash" class="mr-2 h-6 w-6" />
-            <span>Delete Record</span>
-        </RedButton>
-    </div>
-</div>
-
-{#if willDelete}
-    <form
-        method="post"
-        action="?/delete"
-        bind:this={deleteForm}
-        use:enhance={() => {
-            willDelete = false;
-            isLoading = true;
-            return async ({ update }) => {
-                await update();
-                isLoading = false;
-            };
-        }}
-    >
-        <input type="hidden" name="facultyid" value={page.params.facultyid} />
-
-        <DeleteConfirmation
-            onDelete={() => {
-                if (deleteForm) deleteForm.requestSubmit();
-            }}
-            onCancel={() => (willDelete = false)}
-            text="This will delete the current faculty record. Are you sure?"
-        />
-    </form>
-{/if}
-
-{#if isLoading}
-    <LoadingScreen />
 {/if}
