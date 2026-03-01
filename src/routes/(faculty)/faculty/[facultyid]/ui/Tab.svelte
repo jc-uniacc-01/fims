9403db8f0659c2da7fabad7c4776859cd4a7e65b
Eriene Galinato <edgalinato@up.edu.ph>
2026-03-01 13:27:25 +0800

    fix(`Tab`): ensure no tabs are clickable on editing state

--- src/routes/(faculty)/faculty/[facultyid]/ui/Tab.svelte
+++ src/routes/(faculty)/faculty/[facultyid]/ui/Tab.svelte
@@ -3,17 +3,13 @@
     import { viewState } from "../states/view-state.svelte";
     const { href, name } = $props();
 
-    const unselected = $derived(
-        (page.url.pathname === href)
-            ? ''
-            : 'border-b-2'
-    );
-
     let disabled = $derived(page.url.pathname !== href && viewState.isEditing);
 </script>
 
-{#if disabled}
-    <p class="rounded-t-lg border-t-2 border-l-2 border-r-2 border-fims-gray py-1.5 px-6 text-3xl text-fims-gray w-fit text-nowrap border-b-2 border-b-fims-green">{name}</p>
+{#if page.url.pathname === href}
+    <p class="rounded-t-lg border-t-2 border-l-2 border-r-2 border-fims-green py-1.5 px-6 text-3xl text-fims-green hover:bg-fims-green hover:text-white w-fit text-nowrap cursor-pointer">{name}</p>
+{:else if disabled}
+    <p class="rounded-t-lg border-t-2 border-l-2 border-r-2 border-fims-gray py-1.5 px-6 text-3xl text-fims-gray w-fit text-nowrap border-b-2 border-b-fims-green cursor-pointer">{name}</p>
 {:else}
-    <a {href} class="rounded-t-lg border-t-2 border-l-2 border-r-2 border-fims-green py-1.5 px-6 text-3xl text-fims-green hover:bg-fims-green hover:text-white w-fit text-nowrap {unselected}">{name}</a>
+    <a {href} class="rounded-t-lg border-t-2 border-l-2 border-r-2 border-fims-green py-1.5 px-6 text-3xl text-fims-green hover:bg-fims-green hover:text-white w-fit text-nowrap border-b-2">{name}</a>
 {/if}
