fc6e8f2d5c79ec32d0e52948a102ef1467ff3073
Eriene Galinato <edgalinato@up.edu.ph>
2026-03-02 22:27:54 +0800

    feat(faculty/ay/sem/create): add buttons

--- src/routes/(faculty)/faculty/[facultyid]/[ay]/[sem]/create/+page.svelte
+++ src/routes/(faculty)/faculty/[facultyid]/[ay]/[sem]/create/+page.svelte
@@ -1,6 +1,24 @@
 <script>
-    import { setToEdit } from '../../../states/view-state.svelte.js';
+    import Icon from '@iconify/svelte';
+
+    import GreenButton from '$lib/ui/GreenButton.svelte';
+    import RedButton from '$lib/ui/RedButton.svelte';
+
+    import { viewState, setToEdit } from '../../../states/view-state.svelte.js';
 
     // Ensure editing state on navigation here
     setToEdit();
 </script>
+
+{#if viewState.isEditing}
+    <div class="flex items-center gap-2">
+        <GreenButton>
+            <Icon icon="tabler:device-floppy" class="h-5 w-5 mr-2" />
+            <span>Save Record</span>
+        </GreenButton>
+        <RedButton>
+            <Icon icon="tabler:database-off" class="h-5 w-5 mr-2" />
+            <span>Discard Changes</span>
+        </RedButton>
+    </div>
+{/if}
