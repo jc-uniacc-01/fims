352b40e2f91f7ad5735e529e3650a1db96036080
Ericsson Jake B. Camingao <jakecamingaosh@gmail.com>
2026-03-08 21:03:09 +0800

    feat: changelog component and style

---
+++ src/lib/ui/ChangelogList.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+	import ChangelogEntry from '$lib/ui/ChanglogEntry.svelte'
+
+	type ChangelogRecordStructure = {
+			
+	}
+
+	type Props={
+		// basically a callback so that the component can be integrated anywhere since im not sure on future changes of this
+		// should make it easier to implement pagination as well i think
+		changelogFetcher: () => ChangelogRecordStructure[], 
+	};
+
+	let {changelogFetcher}:Props = $props();
+</script>
+
+<div data-testid="changelog-component" class="flex flex-col w-90">
+	<!-- Header -->
+	<div class="bg-fims-green text-white rounded-t-4xl">
+		<h1 class="w-full text-center text-3xl font-semibold py-3">Change Logs</h1>
+	</div>
+
+	<!-- Entries -->
+	<div>
+		<ChangelogEntry timestamp={new Date()} email="test@up.edu.ph" info="Added test"/>
+		<ChangelogEntry timestamp={new Date()} email="test@up.edu.ph" info="Added test"/>
+		<ChangelogEntry timestamp={new Date()} email="test@up.edu.ph" info="Added test"/>
+	</div>
+</div>
---
+++ src/lib/ui/ChanglogEntry.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	type Props = {
+		timestamp:Date,
+		email:string,
+		info:string
+	}
+
+	let {timestamp, email, info}:Props = $props()
+</script>
+
+<!-- 
+notes:
+not entirely sure what's the correct bg color for this component
+-->
+<div data-testid="changelog-entry" class="flex flex-col w-auto border-b-fims-gray border-b-1 px-3 py-2 bg-gray-100">
+	<p class="text-sm">{timestamp.toLocaleDateString()}</p>
+	<p class="text-sm">{email}</p>
+	<p class="text-lg pt-1">{info}</p>
+</div>
