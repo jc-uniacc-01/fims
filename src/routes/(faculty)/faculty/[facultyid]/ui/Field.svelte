9c9ec763338eaee9cbdfbb6f62d9615be3b01032
Eriene Galinato <edgalinato@up.edu.ph>
2026-03-11 15:46:03 +0800

    feat(faculty): flesh out route-specific `Field` component

--- src/routes/(faculty)/faculty/[facultyid]/ui/Field.svelte
+++ src/routes/(faculty)/faculty/[facultyid]/ui/Field.svelte
@@ -1,12 +1,48 @@
 <script lang="ts">
-    let { field="Field", value="Field Value" } = $props();
+    import { onMount } from "svelte";
+
+    import { viewState } from "../states/view-state.svelte";
+
+    interface Props {
+        label: string;
+        name: string;
+        type?: string;
+        defaultValue?: string;
+        colStart?: number;
+        colSpan?: number;
+        immutable?: boolean;
+    }
+
+    const { label, name, type, defaultValue, colStart, colSpan, immutable }: Props = $props();
+
+    let colStartClass = $derived(colStart === undefined ? '' : `col-start-${colStart}`);
+    let colSpanClass = $derived(colSpan === undefined ? '' : `col-span-${colSpan}`);
+
+    // Hacky way of preventing FOUC
+    let domContainer: HTMLLabelElement | null = $state(null);
+    onMount(() => {
+        if (domContainer) domContainer.classList.remove('hidden');
+    });
+
+    // Safelist Tailwind classes
+    // col-span-2
+    // col-span-3
+    // col-span-4
+    // col-span-5
+    // col-span-6
+    // col-span-7
+    // col-span-8
+    // col-span-9
+    // col-span-10
 </script>
 
-<div class="flex w-full">
-    <div class="w-50 content-center flex justify-end px-2 items-center">
-        <p class="w-fit text-align-right">{field}</p>
-    </div>
-    <div class="bg-white px-2 w-100 rounded-sm flex justify-begin items-center">
-        <p class="">{value}</p>
-    </div>
-</div>
+<label class="flex items-center justify-end w-full {colStartClass} {colSpanClass} hidden" bind:this={domContainer}>
+    <span class="w-fit text-right mr-2">{label}</span>
+    <input
+        type={type ?? "text"} {name}
+        class="h-8 w-45 2xl:w-75 rounded-sm border-0 bg-white p-1 placeholder-fims-gray focus:ring-0"
+        placeholder="-"
+        defaultValue={defaultValue ?? ''}
+        disabled={!viewState.isEditing || (viewState.isEditing && immutable && defaultValue !== undefined && defaultValue !== '')}
+    />
+</label>
